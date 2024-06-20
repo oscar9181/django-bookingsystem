4d8ec192bf3cdb8ec6ce0ff87005b1c03771650d
oscar9181 <okolaoscar@gmail.com>
2024-06-20 08:48:33 +0300

    several changes

--- hotel/urls.py
+++ hotel/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('base', views.Home, name = 'base'),
+    # path('base', views.base, name = 'base'),
     path('', views.Home, name = 'home'),
+    path('rooms',views.room_view, name='room'),
     
 ]
 
--- hotel/views.py
+++ hotel/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from .models import Room
 
 # Create your views here.
 def Home(request):
     
     return render(request, 'Home/home.html',{})
+
+
+def room_view(request):
+    available = Room.objects.all()
+    return render(request, 'Home/room.html',{'all': available}) 
 
   
